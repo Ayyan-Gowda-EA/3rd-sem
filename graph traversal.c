af77502a428c3e6782a92ae1ccee0d28c8b6cc5c
Ayyan-Gowda-EA <160257868+Ayyan-Gowda-EA@users.noreply.github.com>
2024-02-18 18:01:22 +0530

    ..

---
+++ graph traversal.c
@@ -0,0 +1,127 @@
+12.b) Develop a menu driven program to implement Graph traversal techniques.
+ 
+#include <stdio.h>
+#include <stdlib.h>
+int N; //Number of Nodes
+//Function for BFS Traversal
+void bfs(int a[][N], int visited[], int start)
+{
+    int Q[N],rear=-1,front=-1; //Initializing Empty Queue
+    int i;
+    //Initializing the Visited Array with 0
+    for(i=0;i<N;i++)
+    visited[i]=0;
+    //Inserting the First node into Queue
+    Q[++rear]=start;
+    ++front;
+    visited[start]=1;
+    while(rear>=front)
+    {
+        start = Q[front++];
+        printf("%c ",start+65);
+        for(i=0;i<N;i++)
+        {
+            if(a[start][i]&&visited[i]==0)
+            {
+                Q[++rear] = i; //Inserting the Adjacent nodes into Queue
+                visited[i] = 1;
+            }
+        }
+    }
+}
+void dfs(int a[][N], int visited[], int start)
+{
+    int S[N],top=-1; //Initializing Empty Stack
+    int i;
+    //Initializing the Visited Array with 0
+    for(i=0;i<N;i++)
+    visited[i]=0;
+    S[++top] = start;
+    visited[start]=1;
+    while(top!=-1)
+    {
+        start = S[top--];
+        printf("%c ",start+65);
+        for(i=0;i<N;i++)
+        {
+            if(a[start][i]&&visited[i]==0)
+            {
+                S[++top] = i; //Inserting the Adjacent node into Stack
+                visited[i] = 1;
+                break;
+            }
+        }
+    }
+}
+void main()
+{
+    int matrix[10][10]; //Declaring adjacency matrix of Graph
+    int visited[10]; //Declaring the Visited Array
+    int choice,i,j;
+    char node; //The node from where traversal is started
+    printf("Menu\n1.Enter the Graph\n2.BFS Traversal\n3.DFS Traversal\n4.Exit\n");
+    for(;;)
+    {
+        printf("\nEnter Your Choice : ");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1 : printf("Enter the Number of Nodes : ");
+                 scanf("%d",&N);
+                 printf("Enter the Adjacency Matrix :\n");
+                 for(i=0;i<N;i++)
+                 for(j=0;j<N;j++)
+                 scanf("%d",&matrix[i][j]);
+                 break;
+        case 2 : printf("The Nodes are named as A,B,C,D...\n");
+                 printf("Enter the node to start with : ");
+                 scanf(" %c",&node);
+                 printf("BFS Traversal : ");
+                 bfs(matrix,visited,node-65);
+                 break;
+        case 3 : printf("The Nodes are named as A,B,C,D...\n");
+                 printf("Enter the node to start with : ");
+                 scanf(" %c",&node);
+                 dfs(matrix,visited,node-65);
+                 break;
+        case 4 : printf("!!THANK YOU!!\n");
+                 exit(0);
+        default: printf("Invalid Choice\n");
+        }
+    }
+}
+
+
+
+
+
+
+
+Output : 
+Menu
+1.Enter the Graph
+2.BFS Traversal
+3.DFS Traversal
+4.Exit
+
+Enter Your Choice : 1
+Enter the Number of Nodes : 6
+Enter the Adjacency Matrix :
+0 1 1 1 1 0
+1 0 0 1 0 0
+1 0 0 1 1 1
+1 1 1 0 0 1
+1 0 1 0 0 0
+0 0 1 1 0 0
+
+Enter Your Choice : 2
+The Nodes are named as A,B,C,D...
+Enter the node to start with : A
+BFS Traversal : A B C D E F
+Enter Your Choice : 3
+The Nodes are named as A,B,C,D...
+Enter the node to start with : A
+A B D C E F
+Enter Your Choice : 4
+!!THANK YOU!!
+
